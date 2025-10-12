a27ea41b1d754c2a6538d5c3c676bd9a42ef758a
Nischal Pandey <nischalpandey0204@gmail.com>
2025-10-12 17:57:00 +0545

    strig manipulation

---
+++ huXn/app.js
@@ -0,0 +1,30 @@
+// Manipulating Strings
+
+let firstName = 'NiSchal ';
+let lastName = 'Pandey';
+
+// 1. Concatenatin
+// let fullname = firstName.concat(lastName);
+
+// 2. Append
+// firstName += "something else";
+// console.log(firstName);
+
+// 3.Length
+// console.log(firstName.length);
+
+// 4.cases
+// console.log(firstName.toLowerCase());
+// console.log(firstName.toUpperCase());
+
+// 5.Slice
+// console.log(firstName.slice(1,3));
+
+// 6. Split & Join
+// console.log(firstName.split('').join(""));
+
+// 7. Includes
+// console.log(firstName.includes('n'));
+
+// 8. Trim
+// console.log(firstName.trim());
