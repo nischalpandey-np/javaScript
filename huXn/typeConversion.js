5dbb47e59befd8d5ae5b798e17e6fa577533509c
Nischal Pandey <nischalpandey0204@gmail.com>
2025-10-13 10:50:45 +0545

    Type conversion in JS

---
+++ huXn/typeConversion.js
@@ -0,0 +1,24 @@
+//Type Conversions
+
+// let money = "50.343";
+
+
+// 1. Convert string to number
+// money = parseInt(money);
+// money = +money;
+// money = Number(money)
+
+
+// 2. Convert number to string
+// money =  money.toString()
+// money = String(money);
+
+
+// 3. Convert string to decimal
+// money = parseFloat (money)
+
+
+
+
+// console.log(money)
+// console.log(typeof(money));
