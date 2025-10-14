3c0066514e2d3f433e5c61e93b6f48448848058e
Nischal Pandey <nischalpandey0204@gmail.com>
2025-10-14 18:59:30 +0545

    callback function

--- javaScript/06_function.js
+++ javaScript/06_function.js
@@ -1,4 +1,5 @@
-//Function
+
+// FUNCTIONS IN JAVASCRIPT
 
 // WHAT IS A FUNCTION?
 // A function is a block of code that performs a specific task.
@@ -8,30 +9,58 @@
 
 
 // FUNCTION DECLARATION
+
 function greet(name) {
-  console.log('Hello, ' + name + '!')
+  console.log(`Hello, ${name}!`)
 }
-greet('Boss')
+// greet('Boss') // Output: Hello, Boss!
 
 
-//  FUNCTION WITH RETURN VALUE
+// FUNCTION WITH RETURN VALUE
+
 function add(a, b) {
   return a + b
 }
 let sum = add(5, 7)
-console.log('Sum is:', sum) // Output: Sum is: 12
+console.log(`Sum is: ${sum}`) // Output: Sum is: 12
 
 
-//  FUNCTION EXPRESSIONS
+// FUNCTION EXPRESSIONS (Anonymous Functions)
+
 // Functions can also be stored in variables.
 const multiply = function (x, y) {
   return x * y
 }
-console.log('Product:', multiply(4, 6)) // Output: Product: 24
+console.log(`Product: ${multiply(4, 6)}`) // Output: Product: 24
 
 
-//  DEFAULT PARAMETERS
+// DEFAULT PARAMETERS
+
 function sayHello(name = 'User') {
-  console.log('Hi ' + name + '!')
+  console.log(`Hello, ${name}!`)
 }
-sayHello() // Output: Hi User!
+sayHello() // Output: Hello, User!
+
+
+// CALLBACK FUNCTIONS
+
+// A callback function is a function passed as an argument
+// to another function, and executed later.
+
+function greetUser(name, callback) {
+  console.log(`Hi, ${name}`)
+  callback()
+}
+
+// The callback function
+function sayBye() {
+  console.log('Goodbye!')
+}
+
+// Call the function with a callback
+greetUser('Boss', sayBye)
+
+// Another example: asynchronous callback using setTimeout
+setTimeout(function () {
+  console.log('This runs after 2 seconds')
+}, 2000)
