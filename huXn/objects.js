8835d728c49c34f508ad334a052623545c2e7068
Nischal Pandey <nischalpandey0204@gmail.com>
2025-10-14 18:05:33 +0545

    object in javaScript

--- huXn/objects.js
+++ huXn/objects.js
@@ -8,3 +8,13 @@ const newObject = {
   location : ['Planet',"Earth"]
 };
 
+// Adding new elements to object
+newObject.status = 'Student'
+
+// Accessing Items from our object
+console.log(newObject.lastName)
+console.log(newObject["firstName"])
+console.log(newObject.status)
+
+// Delete elements from object
+delete newObject.location;
