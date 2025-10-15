cdb31bacddae68bdd7a03f70a772cd5d24b90f11
Nischal Pandey <nischalpandey0204@gmail.com>
2025-10-15 09:59:33 +0545

    formatted with prettier

--- javaScript/08_objectsAndMethods.js
+++ javaScript/08_objectsAndMethods.js
@@ -1,33 +1,33 @@
 // Objects
 
 const newObject = {
-  firstName : "Nischal",
-  lastName : "Pandey",
-  isProgrammer : true ,
+  firstName: 'Nischal',
+  lastName: 'Pandey',
+  isProgrammer: true,
   age: 18,
-  location : ['Planet',"Earth"]
-};
+  location: ['Planet', 'Earth'],
+}
 
 // Adding new elements to object
 newObject.status = 'Student'
 
 // Accessing Items from our object
 console.log(newObject.lastName)
-console.log(newObject["firstName"])
+console.log(newObject['firstName'])
 console.log(newObject.status)
 
 // Delete elements from object
-delete newObject.location;
+delete newObject.location
 
 // Object Methods
 // Methods is a function inside a object
 
-const person ={
-  name : " Nischal",
-  age  : 19,
-  greet : function (){
+const person = {
+  name: ' Nischal',
+  age: 19,
+  greet: function () {
     return `hello my name is ${person.name} & I am ${person.age} years old.`
-  }
+  },
 }
 
 console.log(person.greet())
